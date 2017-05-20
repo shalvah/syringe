70f479c528139dc1f6eef3cc44ade232ba62d99c
Shalvah <diakon.ng@gmail.com>
2017-05-20 19:58:18 +0100

    Add API documentation using Apigen

---
+++ docs/api/elementlist.js
@@ -0,0 +1,2 @@
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c", "Syringe\\Container"], ["c", "Syringe\\Exceptions\\ContainerValueNotFoundException"], ["c", "Syringe\\Exceptions\\UnableToResolveException"], ["c", "Syringe\\Resolver"]];
